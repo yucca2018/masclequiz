fa9aa5f6f4797469570a0a81cff3f0f5a5bc1a54
yucca2018 <98800328+yucca2018@users.noreply.github.com>
2022-02-03 19:23:09 +0900

    initial commit.
    筋肉に関する問題集です。

---
+++ MuscleQuiz/MuscleQuiz/ScoreViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ScoreViewController.swift
+//  MuscleQuiz
+//
+//  Created by user on 2021/03/16.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correct = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scoreLabel.text = "\(correct)問正解"
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toTopButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
